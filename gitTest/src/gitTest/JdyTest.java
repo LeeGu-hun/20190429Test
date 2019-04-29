8d197677e11e9191ce55675417fbb1fd4a8cf77a
ds <ds@ds602-12>
2019-04-29 14:50:57 +0900

    JdyTest

--- gitTest/src/gitTest/JdyTest.java
+++ gitTest/src/gitTest/JdyTest.java
@@ -1,5 +1,12 @@
 package gitTest;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 public class JdyTest {
+	public static void main(String[] args) throws UnknownHostException{
+		InetAddress local = InetAddress.getLocalHost();
+		System.out.println(local.getHostAddress());
+	}
 
 }
